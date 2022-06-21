f122646c4589196ad1d123972133c44835150a90
Dilip chaudhary <dilipchaudhary.yunige@gmail.com>
2022-06-21 10:29:42 +0545

    fixes on outbound apis

--- app/Http/Resources/OutboundResource.php
+++ app/Http/Resources/OutboundResource.php
@@ -17,6 +17,7 @@ class OutboundResource extends JsonResource
         return [
             'id'                        => $this->id,
             'name'                      => $this->name,
+            'warehouse_in_charge'       => $this->warehouse_in_charge,
             'ship_date'                 => $this->ship_date,
             'create_date'               => $this->create_date,
             'estimited_ship_date'       => $this->estimited_ship_date,
@@ -27,7 +28,10 @@ class OutboundResource extends JsonResource
             'completed'                 => $this->completed,
             'special_notes'             => $this->special_notes,
             'fba_entry_date'            => $this->fba_entry_date,
+            'reserve'                   => $this->reserve,
+            'shipping_company'          => ($this->delivery_id != '') ? $this->delivery->name : null,
             'additional_invoice_no'     => $this->additional_invoice_no,
+            'wdata_link'                => ($this->wdata) ? $this->wdata->name : null,
             'created_at'                => $this->created_at,
             'updated_at'                => $this->updated_at,
         ];
