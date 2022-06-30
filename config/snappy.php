7cc3fba21dc1e3f3771f2047a89d860dd2ed7012
Dilip chaudhary <dilipchaudhary.yunige@gmail.com>
2022-06-30 11:57:44 +0545

    fix path of wkhtmltopdf

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
